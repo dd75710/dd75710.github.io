c7d269b2dd632cf3ab90194640c3b365651afc2c
dd75710 <138756944+dd75710@users.noreply.github.com>
2023-07-06 23:55:54 +0900

    Add files via upload

---
+++ JavaScript Playground (3) 3/src/script.js
@@ -0,0 +1,10 @@
+let message = 'Hello world' 
+document.querySelector('#header').innerHTML = message
+console.log(message);
+ let animals = ["cat", "dog", "pig"]; console.log(animals);
+ for (let i = 0; i < animals.length; i++) {
+   console.log( animals[i] ); }
+const items = function(){
+  sum=0;
+  for(i=1; i<=100; i++);
+  sum +=i;}
