db5371cc64073135341cc7aa55648bef4b4da933
dd75710 <138756944+dd75710@users.noreply.github.com>
2023-07-06 19:20:16 +0900

    Add files via upload

---
+++ JavaScript Playground (1)/src/script.js
@@ -0,0 +1,11 @@
+const message = 'Hello world' // Try edit me
+
+// Update header text
+document.querySelector('#header').innerHTML = message
+
+// Log to console
+let title="Hello World";
+console.log(title);
+
+var items = ["dog","cat","bird"];
+console.log(items);
